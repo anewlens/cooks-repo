4eb1a8d8082e2fd084f4fdb2d78487bd5d3f0b71
anewlens <36173414+anewlens@users.noreply.github.com>
2019-12-31 16:53:09 -0500

    Refactored Ratings for single digit rating

--- src/components/Ratings/Ratings.component.jsx
+++ src/components/Ratings/Ratings.component.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React from "react"
 import { ReactComponent as StarFilled } from "../../images/star.svg"
 import { ReactComponent as StarOutline } from "../../images/star-outline.svg"
 
-function Ratings({rating, className}) {
-    return (
-        <span className='Ratings'>
-            {rating.map(num =>
-              num === 1 ? (
-                <StarFilled className={className}/>
-              ) : (
-                <StarOutline className={className}/>
-              )
-            )}
-          </span>
-    )
+function Ratings({ rating, className }) {
+  function ratingArr(num) {
+    const arr = [1, 2, 3, 4, 5]
+
+    return arr.map((val, index) => {
+      if (index + 1 <= num) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+  }
+
+  return (
+    <span className="Ratings">
+      {ratingArr(rating).map(el =>
+        el === 1 ? (
+          <StarFilled className={className} />
+        ) : (
+          <StarOutline className={className} />
+        )
+      )}
+    </span>
+  )
 }
 
 export default Ratings
