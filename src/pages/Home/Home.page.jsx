79013d5d23310777271244812f945ec153070b84
anewlens <36173414+anewlens@users.noreply.github.com>
2019-12-31 16:53:49 -0500

    Style tweak and Preview setup

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,10 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Home.scss"
 import RecipeGrid from "../../components/RecipeGrid/RecipeGrid.component"
+import Preview from "../../components/Preview/Preview.component"
 
 const Home = () => {
+  const [preview, togglePreview] = useState(false)
+
   return (
     <section className="Home">
+      {preview && <Preview />}
       <RecipeGrid />
     </section>
   )
