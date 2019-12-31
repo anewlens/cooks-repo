7197d45690667f951e930409d433255e7006cdfb
anewlens <36173414+anewlens@users.noreply.github.com>
2019-12-31 16:53:29 -0500

    Finish Sample data and map recipes

--- src/RecipeData.js
+++ src/RecipeData.js
@@ -1,20 +1,84 @@
 export default [
-    {
-        id: 1,
-        name: "Garlicky Mashed Potatoes",
-        category: "Sides",
-        rating: 4,
-        dateAdded: '12-1-19',
-        tags: ['garlic', 'time-consuming', 'rich'],
-        ingredients: [
-            {
-                name: 'potatoes',
-                amount: 4,
-                unit: 'lbs',
-                method: 'peeled'
-            }
-        ],
-        notes: ['If it breaks, just mix it up'],
-        steps: ['Bake the potatoes at 350 degrees for 80 minutes.', 'Peel the potatoes while still hot']
-    }
+  {
+    id: 1,
+    name: "Garlicky Mashed Potatoes",
+    category: "Sides",
+    rating: 4,
+    dateAdded: "12-1-19",
+    tags: ["garlic", "time-consuming", "rich"],
+    ingredients: [
+      {
+        name: "potatoes",
+        amount: 4,
+        unit: "lbs",
+        method: "peeled"
+      }
+    ],
+    notes: ["If it breaks, just mix it up"],
+    steps: [
+      "Bake the potatoes at 350 degrees for 80 minutes.",
+      "Peel the potatoes while still hot"
+    ]
+  },
+  {
+    id: 2,
+    name: "Cuban Chicken",
+    category: "Chicken",
+    rating: 3,
+    dateAdded: "12-1-19",
+    tags: ["easy", "lime", "pan"],
+    ingredients: [
+      {
+        name: "chicken breast",
+        amount: 1,
+        unit: "lbs",
+        method: "butterflied"
+      }
+    ],
+    notes: [],
+    steps: [
+      "Pound the chicken breasts thin.",
+      "Throw the lime juice, oil, garlic and spices in a bag."
+    ]
+  },
+  {
+    id: 3,
+    name: "Cream Smothered Steak",
+    category: "Steak",
+    rating: 5,
+    dateAdded: "12-1-19",
+    tags: ["rich", "time-consuming", "pan"],
+    ingredients: [
+      {
+        name: "steak",
+        amount: 2,
+        unit: "lbs"
+      }
+    ],
+    notes: [],
+    steps: [
+      "Pat the steak dry and season generously with salt and pepper",
+      "Preheat the pan and add a tablespoon of canola oil."
+    ]
+  },
+  {
+    id: 4,
+    name: "Chipotle Chicken Copycat",
+    category: "Chicken",
+    rating: 5,
+    dateAdded: "12-1-19",
+    tags: ["easy", "pan", "oven"],
+    ingredients: [
+      {
+        name: "skinless, boneless chicken thighs",
+        amount: 2,
+        unit: "lbs"
+      }
+    ],
+    notes: ["If it breaks, just mix it up"],
+    steps: [
+      "Throw marinade and chicken in ziploc",
+      "Preheat the pan with canola oil."
+    ]
+  }
 ]
--- src/components/RecipeGrid/RecipeGrid.component.jsx
+++ src/components/RecipeGrid/RecipeGrid.component.jsx
@@ -1,45 +1,27 @@
 import React from "react"
 import "./RecipeGrid.scss"
+import recipes from "../../RecipeData"
 import RecipeBox from "../RecipeBox/RecipeBox.component"
 
 const RecipeGrid = () => {
   return (
-    <div className="RecipeGrid">
+    <section className="RecipeGrid">
       <header className="RecipeGrid-header">
         <h1 className="RecipeGrid-title">Your Recipes</h1>
         <input type="text" value="" className="RecipeGrid-search" />
       </header>
       <ul className="RecipeGrid-boxList">
-        <RecipeBox
-          title="Garlicky Mashed Potatoes"
-          category="Sides"
-          dateCreated="12/19"
-          rating={[1, 1, 1, 1, 0]}
-          tags={["garlic", "time-consuming", "rich"]}
-        />
-        <RecipeBox
-          title="Cuban Chicken"
-          category="Chicken"
-          dateCreated="12/19"
-          rating={[1, 1, 1, 0, 0]}
-          tags={["easy", "lime", "pan"]}
-        />
-        <RecipeBox
-          title="Cream Smothered Steak"
-          category="Steak"
-          dateCreated="12/19"
-          rating={[1, 1, 1, 1, 1]}
-          tags={["rich", "time-consuming", "pan"]}
-        />
-        <RecipeBox
-          title="Chipotle Chicken Copycat"
-          category="Chicken"
-          dateCreated="12/19"
-          rating={[1, 1, 1, 1, 1]}
-          tags={["easy", "pan", "oven"]}
-        />
+        {recipes.map(({ name, category, dateAdded, rating, tags }) => (
+          <RecipeBox
+            title={name}
+            category={category}
+            dateCreated={dateAdded}
+            rating={rating}
+            tags={tags}
+          />
+        ))}
       </ul>
-    </div>
+    </section>
   )
 }
 
