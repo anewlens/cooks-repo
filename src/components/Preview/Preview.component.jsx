518e05c1d04bf6eb5ceda47b972758b7136852b9
anewlens <36173414+anewlens@users.noreply.github.com>
2020-01-01 21:34:26 -0500

    Preview Redux and data flow

--- src/components/Preview/Preview.component.jsx
+++ src/components/Preview/Preview.component.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import Ratings from '../Ratings/Ratings.component'
+import React from "react"
+import Ratings from "../Ratings/Ratings.component"
 
-function Preview({title, category, date, rating, }) {
-    return (
-        <div className="Preview">
-            <header className="Preview-header">
-                <h1 className="Preview-title">
-                    {title}
-                </h1>
-                <Ratings rating={rating} className='Preview-rating-star' />
-            </header>
-            <span className="Preview-subheading">
-            <p className="Preview-category">{category}</p>
-            <p className="Preivew-date">Added {date}</p>
-            </span>
-            
-        </div>
-    )
+function Preview({ recipe: { name, category, date, rating } }) {
+  return (
+    <div className="Preview">
+      <header className="Preview-header">
+        <h1 className="Preview-title">{name}</h1>
+        <Ratings rating={rating} className="Preview-rating-star" />
+      </header>
+      <span className="Preview-subheading">
+        <p className="Preview-category">{category}</p>
+        <p className="Preivew-date">Added {date}</p>
+      </span>
+    </div>
+  )
 }
 
 export default Preview
